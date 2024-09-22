4f28fb91fc21edb4477e8ad7c739ea59498e88d2
ai-ahmad <ahmedovalmat8@gmail.com>
2024-09-22 16:00:41 +0500

    test

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html', // Include the main HTML file
+    './src/**/*.{js,ts,jsx,tsx}', // Include all JS, TS, JSX, and TSX files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
